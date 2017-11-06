66bc270fe1a0e70419a1682e8eaca2b09f767b90
Zen Ushida <zen_ushida@jmas.co.jp>
2017-11-06 14:11:23 +0900

    test, release

---
+++ GitRebase/GitRebase/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitRebase
+{
+    class Constant
+    {
+        // test
+        public const string WEB_URL = "Test.com";
+
+        // 本番
+        //public const string WEB_URL = "honban.com";
+    }
+}
